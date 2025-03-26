c6f6db4f09a08c14db16d30900f6cb0e7586af18
PAUL <adebayotoheeb666@gmail.com>
2025-03-26 02:07:03 +0100

    commit

---
+++ controller/lib/Telegram.js
@@ -0,0 +1,35 @@
+const { text } = require('express');
+const { axiosInstance } = require('./axios');
+
+function sendMessage(messageObj, messageText) {
+    return axiosInstance.get('sendMessage', {
+        chat_id: messageObj.chat_id,
+        text: messageText,
+    });
+}
+
+function handleMessage(messageObj) {
+    const messageText = messageObj.text || '';
+
+    if (messageText.charAt(0) === '/') {
+
+        const command = messageText.substr(1);
+        switch (command) {
+            case 'start':
+                //we want to send a welcome message to the user
+                return sendMessage(
+                    messageObj,
+                    "Hi! I'm a bot. I can get you started."
+                );
+            default: 
+                return sendMessage(messageObj,
+                    "Hey hi, I don't recognize that command"
+                );
+        }
+    } else {
+        //we can send the same message back to the user
+        return sendMessage(messageObj, messageText);
+    }
+}
+
+module.exports = { handleMessage };
